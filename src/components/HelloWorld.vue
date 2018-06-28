497db6a121dbb73a379960a6cfb0f52c203faa1a
GiodiCi <g.calaimpirotta@gmail.com>
2018-06-28 23:05:20 +0200

    Hello World - Vue.js Installation

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Mutatio</h1>
+    <h3>UI Photo Editor</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+
+</style>
