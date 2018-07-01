4cfe07783b7b5bf5f622a50f952db41574a12ce1
GiodiCi <g.calaimpirotta@gmail.com>
2018-07-01 21:14:01 +0200

    Added axios API call to Trello.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import TextEditor from './components/TextEditor'
-import Preview from './components/Preview'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import TextEditor from './components/TextEditor';
+import Preview from './components/Preview';
+import TrelloBoard from './components/TrelloBoard';
 
-Vue.component('text-editor', TextEditor)
-Vue.component('preview', Preview)
+Vue.component('text-editor', TextEditor);
+Vue.component('preview', Preview);
+Vue.component('trelloBoard', TrelloBoard);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
