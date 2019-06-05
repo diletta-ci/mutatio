193de512c938890557aed3be2c3c3ab95020b8aa
GiodiCi <diletta@Dilettas-MacBook-Pro.local>
2019-06-05 21:16:09 +0200

    Removing call to Trello component

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import App from './App';
 import router from './router';
 import TextEditor from './components/TextEditor';
 import Preview from './components/Preview';
-import TrelloBoard from './components/TrelloBoard';
 
 Vue.component('text-editor', TextEditor);
 Vue.component('preview', Preview);
-Vue.component('trelloBoard', TrelloBoard);
 
 Vue.config.productionTip = false;
 
